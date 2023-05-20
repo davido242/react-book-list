9eb3db8e19a568dc4a984d628c7bf31e2b38abe7
Monday_David_Sarka <david_md@hamari.agency>
2023-05-20 17:59:27 +0100

    Created states for all inputs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [isbn, setIsbn] = useState("");
+  
   return (
-    <div className="App">
-      <h2>React book list.... 18th May</h2>
+    <div className="wrapper">
+      <h1>BookList App</h1>
+      <p>Add and View your books using local storage</p>
+      <div className='main md:flex mt-8 items-center'>
+        <div className='form-container'>
+          <form  className='form-group'>
+            <label>Title</label>
+            <input type="text" onChange={(e) => setTitle(e.target.value)} value={title} className="form-control" required />
+            <br /><br />
+            <label>Author</label>
+            <input type="text" onChange={(e) => setAuthor(e.target.value)} value={author} className="form-control" required />
+            <br /><br />
+            <label>ISBN#</label>
+            <input type="text" onChange={(e) => setIsbn(e.target.value)} value={isbn} className="form-control" required />
+            <br /><br />
+            <button type='submit' className='bg-green-800 hover:bg-green-600 my-4 py-4'>Add</button>
+          </form>
+        </div>
+        <div className='view-container'>
+        {title}
+        </div>
+      </div>
     </div>
   );
 }
